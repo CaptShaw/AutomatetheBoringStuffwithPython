39e8753bde5b353c550d801038a665aae7ef7e19
CaptShaw <remember_m1@qq.com>
2020-02-15 00:06:20 +0800

    chpt7 mystrip()

--- chpt7.py
+++ chpt7.py
@@ -37,8 +37,18 @@ def pswcheck():
             print('try another')
 
 
+def mystrip(str, tar='\s'):
+    '''写一个函数，它接受一个字符串，做的事情和 strip()字符串方法一样。
+    如果只 传入了要去除的字符串，没有其他参数，那么就从该字符串首尾去除空白字符。
+    否 则，函数第二个参数指定的字符将从该字符串中去除。 '''
+    Regex = re.compile('^[' + tar + ']*|[' + tar + ']*$')
+    print(Regex.sub('', str))
+
 def main():
-    pswcheck()
+    # pswcheck()
+    t = '\siiabdaii\s'
+    mystrip(t, '\\\s')
+    print(t)
 
 
 if __name__ == '__main__':
