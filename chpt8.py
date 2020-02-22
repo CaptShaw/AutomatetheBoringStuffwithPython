dd717fecd4cd98b3f1e873bdd58bd10b7427b558
CaptShaw <remember_m1@qq.com>
2020-02-22 23:11:37 +0800

    add project ccp

--- chpt8.py
+++ chpt8.py
@@ -5,24 +5,27 @@ __author__ = 'CaptShaw'
 
 """
     chapter 8 mad libs
-    usage: 
+ 
 """
 import os, re, shelve, sys
+def mad_libs():
+    # filename = input('please input a filename')
+    filename = 'test.txt'
 
-# filename = input('please input a filename')
-filename = 'test.txt'
+    with open(filename, 'r') as f:
+        content = f.read()
+        print(content)
 
-with open(filename, 'r') as f:
-    content = f.read()
-    print(content)
+    kwRegex = re.compile(r'ADJECTIVE|NOUN|ADVERB|VERB')
+    # x = len(kwRegex.findall(content))
+    # print(x)
+    for i in range(len(kwRegex.findall(content))):
+        relp = input('enter an ' + kwRegex.search(content).group())
+        content = kwRegex.sub(relp, content, count=1)
 
-kwRegex = re.compile(r'ADJECTIVE|NOUN|ADVERB|VERB')
-# x = len(kwRegex.findall(content))
-# print(x)
-for i in range(len(kwRegex.findall(content))):
-    relp = input('enter an ' + kwRegex.search(content).group())
-    content = kwRegex.sub(relp, content, count=1)
+    with open(filename, 'w') as f:
+        f.write(content)
+        print(content)
 
-with open(filename, 'w') as f:
-    f.write(content)
-    print(content)
+if __name__ =='__main__':
+    mad_libs()
